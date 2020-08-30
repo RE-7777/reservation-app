efbf8a4eac59e8e5859bbf6fb226c47b08a546ce
RYOENDO-mn <endo.ryo.mm@gmail.com>
2020-08-30 11:15:36 +0900

    Nodeサーバ構築

---
+++ server/fake-db.js
@@ -0,0 +1,80 @@
+const Product=require('./model/product');
+
+
+class FakeDb{
+
+    constructor(){
+        this.products=[
+            {
+                coverImage: './assets/img/phone-cover.jpg',
+                name: 'Phone XL',
+                price: 799,
+                description: 'A large phone with one of the best screens',
+                heading1:'Sample Text',
+                heading2:'Sample Text',
+                heading3:'Sample Text',
+                headingMessage1:'Sample',
+                headingMessage2:'Sample',
+                headingMessage3:'Sample'
+              },
+              {
+                coverImage: './assets/img/phone-cover.jpg',
+                name: 'Phone Mini',
+                price: 699,
+                description: 'A great phone with one of the best cameras',
+                heading1:'Sample Text',
+                heading2:'Sample Text',
+                heading3:'Sample Text',
+                headingMessage1:'Sample',
+                headingMessage2:'Sample',
+                headingMessage3:'Sample'
+              },
+              {
+                coverImage: './assets/img/phone-cover.jpg',
+                name: 'Phone Standard',
+                price: 299,
+                description: '',
+                heading1:'Sample Text',
+                heading2:'Sample Text',
+                heading3:'Sample Text',
+                headingMessage1:'',
+                headingMessage2:'',
+                headingMessage3:'',
+              },
+              {
+                coverImage: './assets/img/phone-cover.jpg',
+                name: 'Phone Special',
+                price: 999,
+                description: '',
+                heading1:'Sample Text',
+                heading2:'Sample Text',
+                heading3:'Sample Text',
+                headingMessage1:'',
+                headingMessage2:'',
+                headingMessage3:''
+              }
+        ]
+    }
+    async initDB(){
+      await this.cleanDB();
+      this.pushProductsToDb();
+    }
+
+    async cleanDB(){
+      await Product.deleteMany({});
+    }
+    pushProductsToDb() {
+        this.products.forEach(
+            (product) => {
+                const newProduct=new Product(product);
+                newProduct.save();
+            }
+        )        
+    }
+
+    seeDb(){
+        this.pushProductsToDb();
+    }
+}
+
+module.exports =FakeDb;
--- src/app/products.ts
+++ src/app/products.ts
@@ -6,7 +6,10 @@ export const products = [
       description: 'A large phone with one of the best screens',
       heading1:'Sample Text',
       heading2:'Sample Text',
-      heading3:'Sample Text'
+      heading3:'Sample Text',
+      headingMessage1:'Sample',
+      headingMessage2:'Sample',
+      headingMessage3:'Sample'
     },
     {
       coverImage: './assets/img/phone-cover.jpg',
@@ -18,7 +21,7 @@ export const products = [
       heading3:'Sample Text',
       headingMessage1:'Sample',
       headingMessage2:'Sample',
-      headingMessage3:'Sample',
+      headingMessage3:'Sample'
     },
     {
       coverImage: './assets/img/phone-cover.jpg',
@@ -30,7 +33,7 @@ export const products = [
       heading3:'Sample Text',
       headingMessage1:'',
       headingMessage2:'',
-      headingMessage3:'',
+      headingMessage3:''
     },
     {
       coverImage: './assets/img/phone-cover.jpg',
@@ -42,7 +45,7 @@ export const products = [
       heading3:'Sample Text',
       headingMessage1:'',
       headingMessage2:'',
-      headingMessage3:'',
+      headingMessage3:''
     }
   ];
   
